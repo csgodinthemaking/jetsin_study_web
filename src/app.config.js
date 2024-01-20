70bf57ae0690e7a6f0cef4e65379b4cb0cc6cee4
YernarT <toktaryernar@gmail.com>
2024-01-20 21:39:02 +0600

    init: icon config

---
+++ src/app.config.js
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+  nuxtIcon: {
+    size: "20px",
+    class: "itisit-icon",
+    color: "var(--c-text)",
+    aliases: {
+      nuxt: "logos:nuxt-icon",
+    },
+  },
+});
