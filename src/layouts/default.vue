1259aa8ea973f9f3186489e2755ecde5974fcf0d
YernarT <toktaryernar@gmail.com>
2024-01-20 21:41:34 +0600

    init: default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="default-layout">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup />
+
+<style scoped>
+.default-layout {
+  position: relative;
+  background: var(--c-background);
+}
+</style>
