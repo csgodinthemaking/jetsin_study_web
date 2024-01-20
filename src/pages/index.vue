46d9b7e73dc85ea8f5953d56e7174dfb67012c0f
YernarT <toktaryernar@gmail.com>
2024-01-20 21:41:50 +0600

    wip: :construction: index page

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="index-page">
+
+  </main>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { defineComponent } from 'vue';
+
+
+defineComponent({ name: 'IndexPage' });
+</script>
