951ae1bfa4c38489559a7baf9cb8415e642cd9d5
YernarT <toktaryernar@gmail.com>
2024-01-21 23:30:38 +0600

    init: mobile menu

---
+++ src/components/layout/HeaderComp/MobileMenu.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mobile-menu" :class="{ 'mobile-menu--open': props.open }">
+
+    </div>
+</template>
+
+<script setup>
+// Vue
+import { defineComponent, defineProps } from 'vue';
+
+defineComponent({ name: 'MobileMenu' });
+
+const props = defineProps({
+    open: {
+        type: Boolean,
+        required: true
+    }
+});
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/mixins.scss';
+
+.mobile-menu {
+    width: 100%;
+    height: calc(100dvh - 90px);
+    background-color: var(--c-background);
+    opacity: 0;
+    transform: translateX(100%);
+    transition: opacity var(--transition), transform var(--transition);
+    @include positioned($position: fixed, $top: 90px, $left: 0);
+
+    &--open {
+        opacity: 1;
+        transform: translateX(0);
+    }
+}
+</style>
