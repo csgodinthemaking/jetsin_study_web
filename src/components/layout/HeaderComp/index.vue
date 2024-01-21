16db16dcc936467e663157d7b686815733e1beb5
YernarT <toktaryernar@gmail.com>
2024-01-21 16:47:05 +0600

    update: auth modal

--- src/components/layout/HeaderComp/index.vue
+++ src/components/layout/HeaderComp/index.vue
@@ -2,20 +2,27 @@
     <header class="itisit-container header">
         <Logo />
         <NavLink />
-        <ButtonComp class="auth-btn" variant="outlined">Login / Register</ButtonComp>
+        <ButtonComp class="auth-btn" variant="outlined" @click="globalState.authModalVisible = true">
+            Login / Register
+        </ButtonComp>
+        <AuthModal />
     </header>
 </template>
 
 <script setup>
 // Vue
 import { defineComponent } from 'vue';
+// Store
+import { useGlobalState } from '@/store/globalState';
 // Components
 import Logo from './Logo.vue';
 import NavLink from './NavLink.vue';
 import ButtonComp from '@/components/common/ButtonComp.vue';
+import AuthModal from '@/components/common/AuthModal.vue';
 
 defineComponent({ name: 'Logo' });
 
+const globalState = useGlobalState();
 </script>
 
 <style scoped lang="scss">
