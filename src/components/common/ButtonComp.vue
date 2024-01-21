e5551299417d08ed20337cd0ff9eede1a66d902d
YernarT <toktaryernar@gmail.com>
2024-01-21 15:18:57 +0600

    init: Button 组件

---
+++ src/components/common/ButtonComp.vue
@@ -0,0 +1,56 @@
+<template>
+    <a-button class="button" :class="[`button--${props.variant}`]">
+        <slot></slot>
+    </a-button>
+</template>
+
+<script setup>
+// Vue
+import { defineComponent } from 'vue';
+
+defineComponent({ name: 'ButtonComp' });
+
+const props = defineProps({
+    variant: {
+        // 'primary' | 'outlined'
+        type: String,
+        default: 'primary'
+    }
+});
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/mixins.scss';
+
+.button {
+    height: auto;
+    padding: 12px 18px;
+    border: none;
+    border-radius: var(--border-radius);
+    background-color: var(--c-primary);
+    color: #fff;
+    cursor: pointer;
+    transition: color var(--transition), background-color var(--transition);
+
+    :deep(span) {
+        font-weight: 500;
+    }
+
+    &:hover {
+        color: #fff;
+    }
+
+    &--outlined {
+        color: var(--c-text);
+        border: 2px solid var(--c-border);
+        background-color: transparent;
+
+        &:hover {
+            color: #fff;
+            border-color: var(--c-primary);
+            background-color: var(--c-primary);
+        }
+    }
+}
+</style>
