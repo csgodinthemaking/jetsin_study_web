9ce16e331b489c1e0f6b023fd6330c1851d26bac
YernarT <toktaryernar@gmail.com>
2024-01-21 15:19:34 +0600

    init: Header

---
+++ src/components/layout/HeaderComp/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="itisit-container header">
+        <Logo />
+        <NavLink />
+        <ButtonComp class="auth-btn" variant="outlined">Login / Register</ButtonComp>
+    </header>
+</template>
+
+<script setup>
+// Vue
+import { defineComponent } from 'vue';
+// Components
+import Logo from './Logo.vue';
+import NavLink from './NavLink.vue';
+import ButtonComp from '@/components/common/ButtonComp.vue';
+
+defineComponent({ name: 'Logo' });
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/mixins.scss';
+
+.header {
+    padding-top: 24px;
+    padding-bottom: 24px;
+    @include flex($alignItems: center, $gap: 16px);
+
+    .auth-btn {
+        margin-left: auto;
+    }
+}
+</style>
