18fe4f90b51ed217bbab2d6332119cc01de4bf06
YernarT <toktaryernar@gmail.com>
2024-01-21 15:19:13 +0600

    init: Logo

---
+++ src/components/layout/HeaderComp/Logo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="logo">
+        <img src="/favicon.png" alt="Jetsin study">
+        <div class="col">
+            <span>Jestin</span>
+            <span>Edu</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// Vue
+import { defineComponent } from 'vue';
+
+defineComponent({ name: 'Logo' });
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/mixins.scss';
+
+.logo {
+    cursor: pointer;
+    @include flex($alignItems: center, $gap: 8px);
+
+    img {
+        width: 60px;
+        height: 60px;
+        object-fit: cover;
+    }
+
+    .col {
+        @include flex($direction: column, $alignItems: center, $gap: 1px);
+
+        span {
+            font-weight: 500;
+            line-height: 1;
+
+            &:first-child {
+                color: var(--c-primary);
+            }
+        }
+    }
+}
+</style>
