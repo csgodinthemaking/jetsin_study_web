b4dc750ab4d47252fed075af8ebf6777d9644f94
YernarT <toktaryernar@gmail.com>
2024-01-22 23:00:05 +0600

    update: Banner Section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <main class="index-page">
-
+    <BannerSection />
   </main>
 </template>
 
 <script setup lang="ts">
 // Vue
 import { defineComponent } from 'vue';
-
+// Components
+import BannerSection from '@/components/index_page/BannerSection.vue';
 
 defineComponent({ name: 'IndexPage' });
 </script>
