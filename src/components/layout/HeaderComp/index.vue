d5e7ead6f139ad1b402b5309f8b6ed1d803e399e
YernarT <toktaryernar@gmail.com>
2024-01-22 22:48:49 +0600

    update: keep fixed

--- src/components/layout/HeaderComp/index.vue
+++ src/components/layout/HeaderComp/index.vue
@@ -5,36 +5,75 @@
         <ButtonComp class="auth-btn" variant="outlined" @click="globalState.authModalVisible = true">
             Login / Register
         </ButtonComp>
+        <ButtonComp icon variant="outlined" class="toggle-btn" @click="isMobileMenuOpened = !isMobileMenuOpened">
+            <Icon v-if="!isMobileMenuOpened" name="ri-menu-fill" />
+            <Icon v-else name="ooui:close" />
+        </ButtonComp>
         <AuthModal />
+        <MobileMenu :open="isMobileMenuOpened" />
     </header>
+    <div class="header-helper"></div>
 </template>
 
 <script setup>
 // Vue
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 // Store
 import { useGlobalState } from '@/store/globalState';
 // Components
 import Logo from './Logo.vue';
 import NavLink from './NavLink.vue';
+import MobileMenu from './MobileMenu.vue';
 import ButtonComp from '@/components/common/ButtonComp.vue';
 import AuthModal from '@/components/common/AuthModal.vue';
 
 defineComponent({ name: 'Logo' });
 
 const globalState = useGlobalState();
+const isMobileMenuOpened = ref(false);
 </script>
 
 <style scoped lang="scss">
 @import '@/assets/style/mixins.scss';
 
 .header {
-    padding-top: 24px;
-    padding-bottom: 24px;
+    width: 100%;
+    height: 104px;
+    background-color: transparent;
     @include flex($alignItems: center, $gap: 16px);
+    @include positioned($position: fixed, $top: 0, $left: 0, $zIndex: 40);
 
     .auth-btn {
         margin-left: auto;
+    }
+
+    .toggle-btn {
+        display: none;
+        margin-left: auto;
+        padding: 8px 12px;
+    }
+
+    /* Responsive */
+    @media screen and (max-width: 800px) {
+        height: 90px;
+
+        &>.nav-link,
+        &>.auth-btn {
+            display: none;
+        }
+
+        .toggle-btn {
+            display: flex;
+        }
+    }
+}
+
+.header-helper {
+    height: 104px;
+
+    /* Responsive */
+    @media screen and (max-width: 800px) {
+        height: 90px;
     }
 }
 </style>
